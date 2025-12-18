bfe08e1526548af28e32c5f9f400bbd3203634fb
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-12-18 13:55:23 +0100

    finalizzazione snack-4

---
+++ snack-4/main.js
@@ -0,0 +1,66 @@
+// Creare un array (ages) che contiene le età degli autori dei libri.
+// Calcola la somma delle età (agesSum) usando reduce.
+// Stampa in console l’età media degli autori dei libri.
+
+
+const books = [
+  { 
+	  title: "React Billionaire", 
+	  pages: 250, 
+	  author: {
+		  name: 'Alice',
+		  age: 35
+	  },
+	  available: false,
+	  price: '101€',
+	  tags: ['advanced', 'js', 'react', 'senior']
+  },
+  { 
+	  title: "Advanced JS", 
+	  pages: 500, 
+	  author: {
+		  name: 'Bob',
+		  age: 20
+	  },
+	  available: true,
+	  price: '25€',
+	  tags: ['advanced', 'js', 'mid-senior']
+  },
+  { 
+	  title: "CSS Secrets", 
+	  pages: 320, 
+	  author: {
+		  name: 'Alice',
+		  age: 17
+	  },
+	  available: true,
+	  price: '8€',
+	  tags: ['html', 'css', 'junior']
+  },
+  { 
+	  title: "HTML Mastery", 
+	  pages: 200, 
+	  author: {
+		  name: 'Charlie',
+		  age: 50
+	  },
+	  available: false,
+	  price: '48€',
+	  tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+//1
+const ages = books.map((b => b.author.age));
+console.log(ages);
+
+//2
+const agesSum = ages.reduce((acc, curr) => {
+	return acc + curr;
+}, 0);
+console.log(agesSum);
+
+//3
+const etaMedia = agesSum / ages.length;
+console.log(etaMedia);
+
